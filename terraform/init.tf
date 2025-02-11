58cb5eede5b26a8f1c23abdccdec72f5636b3310
Maroline Johnson <maroline.johnson@gmail.com>
2025-02-11 13:39:47 -0600

    Update init.tf

--- terraform/init.tf
+++ terraform/init.tf
@@ -1,12 +1,28 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.86.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.6"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.2"
     }
   }
 }
 
 provider "aws" {
+  region = "us-east-1"
+}
+
+provider "tls" {
+  # Configuration options
+}
+
+provider "local" {
   # Configuration options
 }
